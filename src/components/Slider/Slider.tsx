e3e448d05ae6dac2d2715a3e5c54cea678f75fd4
Reginaldo Paiva de Araujo Filho <Regifilho06@gmail.com>
2023-02-03 11:43:19 +0000

    fix minor bug

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,57 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import { useTheme } from 'styled-components';
+import React, { useEffect, useState } from "react";
+import { useTheme } from "styled-components";
 // import { useTheme } from '../../context/ThemeContext';
-import { Background, SliderObject } from './Slider.styles';
+import { Background, SliderInput, SliderObject } from "./Slider.styles";
 
 const Slider = () => {
-    const [sliderState, setSliderState] = useState({ maxSlide: 99999, slided: 50 });
-    const [holdState, setHoldState] = useState(false);
+  const [sliderState, setSliderState] = useState({
+    slided: 0,
+    actionSlideValue: 80,
+    maxValue: 100,
+    minValue: 0,
+  });
+  const [holdState, setHoldState] = useState(false);
 
-    useEffect(() => {
-        try {
-            setSliderState({ ...sliderState, maxSlide: document.getElementById('body').clientWidth });
-        } catch (e) {}
-    }, []);
-
-    useEffect(() => {
-        const Update = () => {
-            setSliderState({ ...sliderState, slided: sliderState.slided - 1 });
-        };
-
-        const interval = setInterval(Update, 500);
-
-        return () => clearInterval(interval);
-    }, []);
-
-    function OnChangeValue({ target }) {
-        if (holdState == true) {
-            setSliderState({ ...sliderState, slided: target.value });
-        }
+  function OnChangeValue({ target }) {
+    if (holdState == true) {
+      setSliderState({ ...sliderState, slided: target.value });
     }
+  }
 
-    function GetMouse(state) {
-        setHoldState(state);
-    }
-    return (
-        <div>
-            <input
-                type="range"
-                min="1"
-                max="100"
-                onMouseDown={() => GetMouse(true)}
-                onMouseUp={() => GetMouse(false)}
-                value={sliderState.slided}
-                onChange={OnChangeValue}
-                id="myRange"
-            />
-        </div>
-    );
+  function GetMouse(state) {
+    setHoldState(state);
 
-    return (
-        <Background id="body">
-            <SliderObject onMouseDown={() => {}}>{'>'}</SliderObject>
-        </Background>
-    );
+    if (!state)
+      if (sliderState.slided < sliderState.actionSlideValue)
+        setSliderState({ ...sliderState, slided: 0 });
+      else setSliderState({ ...sliderState, slided: 100 });
+  }
+
+  useEffect(() => {
+    if (sliderState.slided == 100) console.log("Do action!");
+  }, [sliderState]);
+
+  return (
+    <Background>
+      <SliderInput
+        type="range"
+        min={sliderState.minValue}
+        max={sliderState.maxValue}
+        onMouseDown={() => GetMouse(true)}
+        onMouseUp={() => GetMouse(false)}
+        value={sliderState.slided}
+        onChange={OnChangeValue}
+        id="myRange"
+      />
+    </Background>
+  );
+
+  return (
+    <Background id="body">
+      <SliderObject onMouseDown={() => {}}>{">"}</SliderObject>
+    </Background>
+  );
 };
 
 export default Slider;
